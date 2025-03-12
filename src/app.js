c7ca6be2b512ae75377d9f7179d28379f84e38a2
Juan-Carlos-Rizo-Mendoza <85327@alumnos.utleon.edu.mx>
2025-03-11 22:50:50 -0600

    subir archivos

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import { pool } from '../db/db.js';
+import {PORT} from '../config.js';
+
+const app = express();
+const port = PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/ping', async (req, res) => {
+  try {
+    const [result]= await pool.query('SELECT "Hello world" AS RESULT');
+    res.json(result);
+  } catch (error) {
+    console.error('Database connection error:', error);
+    res.status(500).send('Database connection error');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const [result] = await pool.query('SELECT * FROM usuario');
+    res.json(result);
+  } catch (error) {
+    console.error('Database connection error:', error);
+    res.status(500).send('Database connection error');
+  }
+});
+
+//login morgan winston bunyan pino log4js
---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const PORT = ProcessingInstruction.env.PORT || 3000;
+
+export const DB_HOST = ProcessingInstruction.env.DB_HOST || 'trolley.proxy.rlwy.net';
+
+export const DB_USER = ProcessingInstruction.env.DB_USER || 'root';
+
+export const DB_PASSWORD = ProcessingInstruction.env.DB_PASSWORD || 'zNzVJbRqIvstlDeLpjwYDdkcfFlrLljX';
+
+export const DB_DATABASE = ProcessingInstruction.env.DB_DATABASE || 'railway';
+
+export const DB_PORT = ProcessingInstruction.env.DB_PORT || 26262;
